18a07b48d6bfce91a49b2babe6c75d001686eae0
Samueelx <gsammygitau@gmail.com>
2022-12-13 14:37:05 +0300

    fixed bug caused by innerHTML.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const MOVIES_URL = 'http://localhost:3000/films';
-const mainContainer = document.querySelector('.main-container');
+const MOVIES_URL = "http://localhost:3000/films";
+const mainContainer = document.querySelector(".main-container");
 
 const firstMovie = (film) => {
-    /**create a div where our first movie shall be rendered */
-    const mainDiv = document.createElement('div');
-    mainDiv.classList.add('main-movie');
+  /**create a div where our first movie shall be rendered */
+  const mainDiv = document.createElement("div");
+  mainDiv.classList.add("main-movie");
 
-    /**Calculate the number of available tickets */
-    let availableTickets = film.capacity - film.tickets_sold;
+  /**Calculate the number of available tickets */
+  let availableTickets = film.capacity - film.tickets_sold;
 
-    mainDiv.innerHTML = `
+  mainDiv.innerHTML = `
         <h3 class="pt-5 pb-3">${film.title}</h3>
         <img src="${film.poster}" alt="Main Movie" class="img-fluid" id="mainMovie">
         <div class="movie-details mt-3">
@@ -24,66 +24,68 @@ const firstMovie = (film) => {
             <button type="button" class="btn btn-outline-dark mt-3 mb-5">Buy Tickets</button>
         </div>
     `;
-    /**Append the div into out main comtainer div */
-    mainContainer.appendChild(mainDiv);
+  /**Append the div into out main comtainer div */
+  mainContainer.appendChild(mainDiv);
 
-    /**Purchase tickets and update DOM */
-    const buyTicket = mainDiv.querySelector('.btn-outline-dark');
-    buyTicket.addEventListener('click', () => {
-        const tickets = mainDiv.querySelector('p#tickets');
-        if(availableTickets > 0){
-            availableTickets -=1;
-            tickets.textContent = `Available tickets: ${availableTickets}`;
-        }
-        else buyTicket.textContent = 'SOLD OUT!';
-    });
-}
+  /**Purchase tickets and update DOM */
+  const buyTicket = mainDiv.querySelector(".btn-outline-dark");
+  buyTicket.addEventListener("click", () => {
+    const tickets = mainDiv.querySelector("p#tickets");
+    if (availableTickets > 0) {
+      availableTickets -= 1;
+      tickets.textContent = `Available tickets: ${availableTickets}`;
+    } else buyTicket.textContent = "SOLD OUT!";
+  });
+};
 
 /**Create and render the menu list of films */
-const createFilms = (film) => {
-    const menuDiv = document.querySelector('div.movies-menu');
-    const ul = document.querySelector('#films');
-    console.log('before li');
-    ul.innerHTML = `
-        <li class="mt-2 mb-2 film item">
-            <div class="card">
-                <img src="${film.poster}" alt="" class="card-img-top img-fluid">
-                <div class="card-body">
-                    <h4 class="card-title">${film.title}</h4>
-                    <button type="button" class="btn btn-outline-dark">View Details</button>
-                </div>
-            </div>
-        </li>
-    `;
-    console.log('after li');
-    // menuDiv.appendChild(ul);
-    // mainContainer.appendChild(menuDiv);
+const createFilms = (films) => {
+  const menuDiv = document.querySelector("div.movies-menu");
+  const ul = document.querySelector("#films");
 
-}
+  films.forEach((film) => {
+    console.log("before li");
+    ul.insertAdjacentHTML(
+      "afterbegin",
+      `<li class="mt-2 mb-2 film item">
+    <div class="card">
+        <img src="${film.poster}" alt="" class="card-img-top img-fluid">
+        <div class="card-body">
+            <h4 class="card-title">${film.title}</h4>
+            <button type="button" class="btn btn-outline-dark">View Details</button>
+        </div>
+    </div>
+    </li>`
+    );
+    console.log("after li");
+  });
+  // menuDiv.appendChild(ul);
+  // mainContainer.appendChild(menuDiv);
+};
 
 /**Fetch the first movie */
 const fetchFirst = (MOVIES_URL) => {
-    fetch(`${MOVIES_URL}/1`)
-    .then(response => response.json())
+  fetch(`${MOVIES_URL}/1`)
+    .then((response) => response.json())
     .then((film) => {
-        firstMovie(film);
+      firstMovie(film);
     });
-}
+};
 
 /**Fetch all films*/
 const fetchAll = (MOVIES_URL) => {
-    fetch(MOVIES_URL)
-    .then(response => response.json())
+  fetch(MOVIES_URL)
+    .then((response) => response.json())
     .then((films) => {
-        // console.log(films);
-        films.forEach((film) => {
-            createFilms(film);
-        });
+      // console.log(films);
+      createFilms(films);
+      // films.map((film) => {
+      //     createFilms(film);
+      // });
     });
-}
+};
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetchAll(MOVIES_URL);
-    fetchFirst(MOVIES_URL);
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fetchAll(MOVIES_URL);
+  fetchFirst(MOVIES_URL);
+});
